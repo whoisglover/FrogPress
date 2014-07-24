7699ff26e7ee437907d8eae52a6a5f063f163b80
Danny Glover <djglover00@gmail.com>
2014-07-24 15:55:16 -0700

    Initial commit, Rails 4.0.0 Ruby 2.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FrogPress::Application.config.secret_key_base = 'e37dd5c6e8d55cad173640eb75756390c84b99fe071662662fb69caedb3ebb53c30364834a4f6acb199c2d805b40c1bca54a5da189b3613a8793c085e0b7cc37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FrogPress::Application.config.session_store :cookie_store, key: '_FrogPress_session'
