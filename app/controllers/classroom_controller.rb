9fff5ce1d91786074ac09aef8d4336f2693b206f
& <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-24 22:36:02 -0700

    stopping point for thursday night route creation (classroom)
    Matt and Danny have the classroom#index route started and redirecting anyone not logged in. This commit is a work in progress commit and should not be merged.

--- app/controllers/classroom_controller.rb
+++ app/controllers/classroom_controller.rb
@@ -1,8 +1,12 @@
 class ClassroomController < ApplicationController
+  before_filter :authenticate_user!
   protect_from_forgery
 
   def index
-    @classrooms = Classroom.all
+    p 'hello'
+    # if current_user.user_type != 'teacher'
+      # redirect_to root_path, status: 303
+    # end
   end
 
 end
