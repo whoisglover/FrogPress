65670816fdb0eaa6d3112bedd8a5f71ccc0814a8
& <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-24 22:36:02 -0700

    stopping point for thursday night route creation (classroom)
    Matt and Danny have the classroom#index route started and redirecting anyone not logged in. This commit is a work in progress commit and should not be merged.

--- spec/controllers/classroom_controller_spec.rb
+++ spec/controllers/classroom_controller_spec.rb
@@ -3,24 +3,20 @@ require 'spec_helper'
 describe ClassroomController do
   describe '#index' do
     it 'redirects to home if user is not logged in' do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+    it 'return all classrooms for current teacher' do
       #arrange
-      student = FactoryGirl.create(:student)
-      current_user = student
-      current_user = nil || current_user.user_type == 'teacher'
+      teacher = FactoryGirl.create(:teacher)
+      classroom = FactoryGirl.create(:classroom)
+      classroom.teacher_id = teacher.id
+      current_user = teacher
       #act
       get :index
       #assert
-      expect(response).to redirect_to('/')
+      expect(assigns(:classrooms)).to include(classroom)
     end
-    # it 'return all classrooms for current teacher' do
-    #   #arrange
-    #   teacher = FactoryGirl.create(:teacher)
-    #   current_user = teacher
-    #   #act
-    #   get :index
-    #   #assert
-
-    # end
   end #end index
 
   # describe '#create' do
--- spec/factories/classrooms.rb
+++ spec/factories/classrooms.rb
@@ -2,5 +2,8 @@
 
 FactoryGirl.define do
   factory :classroom do
+    name "Period 1"
+    grade_level 5
+    join_code "hello"
   end
 end
