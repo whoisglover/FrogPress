e7e4acec44719db417d4f75ab2bc4fa5280f8059
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-26 00:25:04 -0700

    created user#show routes, all tests pass

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe UsersController do
+  describe '#show' do
+    it 'should redirect to sign in if no user signed in' do
+      #arrange
+      #act
+      get :show, id: 4
+      #assert
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it 'should redirect to correct profile if not a teacher' do
+      #arrange
+      login = login_student
+      #grab teacher id from login_teacher helper function
+      student_id = login[0][0]
+      #grab the student using the student id
+      student = User.find_by_id(student_id)
+      #act
+      get :show, id: 4
+      #assert
+      expect(response).to redirect_to(user_path(student.id))
+    end
+
+    it 'should return the information for the user' do
+      #arrange
+      login = login_teacher
+      #grab teacher id from login_teacher helper function
+      teacher_id = login[0][0]
+      #grab the teacher using the teacher id
+      teacher = User.find_by_id(teacher_id)
+      #act
+      get :show, id: teacher.id
+      #assert
+      expect(assigns(:user)).to eq(teacher)
+    end
+  end
+end
