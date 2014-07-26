eaa9466ee3d78f6cc47212e4a134b67f821ff3c0
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-26 00:17:34 -0700

    created tests and delete and update routes

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -3,9 +3,6 @@ class AssignmentController < ApplicationController
   before_filter :verify_user
   protect_from_forgery
 
-  def show
-    @assignment = Assignment.find_by_id(params[:id])
-  end
 
   def new
     # pass classroom id to form. Pass from form in to create route.
@@ -13,9 +10,32 @@ class AssignmentController < ApplicationController
 
   def create
     assignment_hash = params[:assignment]
-    assignment_hash_hash = assignment_hash.to_hash
-    assignment_hash_hash.symbolize_keys!
-    assignment = Assignment.create(assignment_hash_hash)
+    assignment_hash = assignment_hash.to_hash
+    assignment_hash.symbolize_keys!
+    assignment = Assignment.create(assignment_hash)
+    redirect_to (classroom_path(assignment.classroom_id))
+  end
+
+  def show
+    @assignment = Assignment.find_by_id(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    assignment_hash = params[:assignment]
+    assignment_hash = assignment_hash.to_hash
+    assignment_hash.symbolize_keys!
+    assignment_to_change = Assignment.find_by_id(params[:id])
+    assignment_to_change.update(assignment_hash)
+    redirect_to (classroom_path(assignment_to_change.classroom_id))
+
+  end
+
+  def destroy
+    assignment = Assignment.find(params[:id])
+    assignment.destroy
     redirect_to (classroom_path(assignment.classroom_id))
   end
 
