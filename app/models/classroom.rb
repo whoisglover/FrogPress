699edb40261bfe53127c8b08d844b95f5a813d61
brennontwilliams <brennonw@gmail.com>
2014-07-26 15:24:17 -0700

    Student can now add themselves to class
    Both student and teacher forms on the classroom create page are functional. Created add_student_to_class method in the classroom model. Classroom controller's create method either creates class or adds user to class depending on current user type.

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -7,4 +7,9 @@ class Classroom < ActiveRecord::Base
   validates :join_code, presence: true
   validates :join_code, uniqueness: true
 
+  def self.add_student_to_class(user_id, join_code)
+    @classroom_to_add_student = Classroom.find_by_join_code(join_code)
+    @classroom_to_add_student.users << User.find_by_id(user_id)
+  end
+
 end
