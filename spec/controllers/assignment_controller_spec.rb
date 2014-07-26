49ba69f2481668e91dd83adcb81634a23db56407
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-26 00:17:34 -0700

    created tests and delete and update routes

--- spec/controllers/assignment_controller_spec.rb
+++ spec/controllers/assignment_controller_spec.rb
@@ -1,11 +1,32 @@
 require 'spec_helper'
 
 describe AssignmentController do
-  describe "#show" do
-
-    it 'directs to add assignment if user is teacher' do
+  describe "#create" do
+    let(:assignment_params) { FactoryGirl.attributes_for(:assignment)}
+    let(:assignment_invalid_params) { FactoryGirl.attributes_for(:assignment_bad_params)}
+    it "creates a new assignment" do
       # arrange
-        login = login_teacher
+        login_teacher
+        classroom = FactoryGirl.create(:classroom)
+        assignment = FactoryGirl.create(:assignment)
+        classroom.assignments << assignment
+      #act/assert
+        expect{
+        post :create, assignment: assignment_params}.to change{Assignment.count}.by(1)
+    end
+
+    it 'does not create assignment without a classroom id' do
+      login_teacher
+      expect{
+        post :create, assignment: assignment_invalid_params}.to change{Assignment.count}.by(0)
+    end
+  end
+
+
+  describe "#show" do
+    it 'adds assignment if user is teacher' do
+      # arrange
+        login_teacher
         classroom = FactoryGirl.create(:classroom)
         assignment = FactoryGirl.create(:assignment)
         classroom.assignments << assignment
@@ -16,27 +37,39 @@ describe AssignmentController do
     end
   end
 
-  describe "#create" do
+  describe "#update" do
+    let(:assignment) { FactoryGirl.create(:assignment) }
     let(:assignment_params) { FactoryGirl.attributes_for(:assignment)}
-    let(:assignment_invalid_params) { FactoryGirl.attributes_for(:assignment_bad_params)}
-    it "creates a new assignment" do
+    it "updates the specified assignment with new parameters in the database" do
       # arrange
-        login = login_teacher
-        classroom = FactoryGirl.create(:classroom)
-        assignment = FactoryGirl.create(:assignment)
-        classroom.assignments << assignment
-      #act/assert
-        expect{
-        post :create, assignment: assignment_params}.to change{Assignment.count}.by(1)
+        login_teacher
+        original_assignment = Assignment.create(title: "test title", content: "test_content", due_date: "11-01-2014")
+      # act
+        put :update, { id: assignment.id, assignment: assignment_params }
+        assignment.reload
+      # assert
+        expect(assignment.title).to eq(assignment_params[:title])
     end
 
-    it 'does not create assignment without a classroom id' do
-      login = login_teacher
-      expect{
-        post :create, assignment: assignment_invalid_params}.to change{Assignment.count}.by(0)
+    it "should redirect the user to the classroom_path" do
+      login_teacher
+      put :update, {id: assignment.id, assignment: assignment_params }
+      expect(response).to redirect_to(classroom_path(assignment.classroom_id))
     end
-
   end
 
+  describe "#destroy" do
+    let(:assignment) { FactoryGirl.create(:assignment) }
+    it 'should remove the assignment from the database' do
+      login_teacher
+      assignment
+      expect {delete :destroy, id: assignment.id}.to change{Assignment.count}.by(-1)
+    end
 
+    it "should redirect the user to the classroom_path" do
+      login_teacher
+      delete :destroy, id: assignment.id
+      expect(response).to redirect_to(classroom_path(assignment.classroom_id))
+    end
+  end
 end
