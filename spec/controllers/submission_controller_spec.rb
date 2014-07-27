821a21b1aef72ab3f8e3074114371e68623627ca
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-26 19:47:44 -0700

    - changed location of Database cleaner  - created submission create route and test  - created submission show / update route and test

--- spec/controllers/submission_controller_spec.rb
+++ spec/controllers/submission_controller_spec.rb
@@ -1,22 +1,50 @@
 require 'spec_helper'
 
 describe SubmissionController do
-  describe "#create" do
+  describe '#create' do
     let(:submission_params) { FactoryGirl.attributes_for(:submission) }
     it "creates a new submission" do
       #arrange
         login = login_student
-        student_id = login[0][0]
-        student = User.find_by_id(student_id)
+        user_id = login[0][0]
+        user = User.find_by_id(user_id)
         classroom = FactoryGirl.create(:classroom)
         assignment = FactoryGirl.create(:assignment)
         classroom.assignments << assignment
-        # submission = FactoryGirl.create(:submission)
-        # assignment.submissions << submission
-        # student.submissions << submission
+        params = {user_id: user_id, submission: submission_params}
       #act/assert
-        expect{
-          post :create, submission: submission_params, assignment: assignment.id}.to change{Submission.count}.by(1)
+        expect{post :create, params}.to change{Submission.count}.by(1)
+    end
+  end
+
+  describe '#show' do
+    it "returns the correct assignment" do
+      login = login_student
+      #arrange
+      assignment = FactoryGirl.create(:assignment)
+      #act
+      get :show, id: assignment.id
+      #assert
+      expect(assigns(:assignment)).to eq(assignment)
+    end
+  end
+
+  describe '#update' do
+    it 'updates the assignment in the database' do
+    #arrange
+      login = login_student
+      user_id = login[0][0]
+      user = User.find_by_id(user_id)
+      submission = FactoryGirl.create(:submission)
+      assignment = FactoryGirl.create(:assignment)
+      assignment.submissions << submission
+      subid = submission.id
+      new_params = {sub_title: 'dannys paper'}
+    #act
+      patch :update, id: subid, new_params: new_params
+      submission.reload
+    #assert
+    expect(submission.sub_title).to eq('dannys paper')
     end
   end
 end
--- spec/factories/submissions.rb
+++ spec/factories/submissions.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
     sub_title "My Summer at DevBootcamp"
     sub_content "I made so many friends at DBC.  I was nervous because I am a bit shy"
     status "draft"
+    assignment_id 1
   end
 end
 
