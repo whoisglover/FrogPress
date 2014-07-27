fe8131f80e3c586c243e62f3de7e4cd0d1df5d67
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-27 01:02:34 -0700

    work in progress commit, setting up routes, controller, and tests for feedback

---
+++ spec/controllers/feedback_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe FeedbackController do
+  describe "#index" do
+    it 'should return all feedback'
+  end
+
+  describe "#show" do
+
+  end
+
+  describe "#create" do
+
+  end
+
+  describe "#update" do
+
+  end
+
+  describe "#destroy" do
+
+  end
+
+end
