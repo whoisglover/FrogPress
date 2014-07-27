ad452e7edd9884a5cb3ff29d56d2878d87c1c9d2
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-26 17:48:07 -0700

    Created teacher view of classroom/:id page
    This includes helper methods in the classroom model to get things like recent assignments and class roster

--- app/helpers/classroom_helper.rb
+++ app/helpers/classroom_helper.rb
@@ -1,2 +1,10 @@
 module ClassroomHelper
+
+# create new assignment
+# list of pending assignments
+# list of past due assignments
+# list of student in the classroom
+# delete a student from a classroom
+
+
 end
--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -7,4 +7,28 @@ class Classroom < ActiveRecord::Base
   validates :join_code, presence: true
   validates :join_code, uniqueness: true
 
+  def all_assignments
+    @these_assignments = self.assignments
+  end
+
+  def pending_assignments
+    @these_pending_assignments = self.all_assignments.where("due_date >= ?", Date.today)
+    @these_pending_assignments = @these_pending_assignments.sort_by(&:due_date).reverse
+  end
+
+  def past_due_assignments
+    @these_past_due_assignments = self.all_assignments.where("due_date < ?", Date.today)
+    @these_past_due_assignments = @these_past_due_assignments.sort_by(&:due_date).reverse
+  end
+
+  def student_roster
+    @these_students = self.users.where("user_type = ?", "student")
+  end
+
+
+
+  # def create_assignment
+
+  # end
 end
+
