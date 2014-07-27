b604fcbe1887732733ceb7eb9485919075dc2694
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-26 22:54:32 -0700

    Fixed 'add class' loophole that allowed the same class to be added twice'

--- app/helpers/classroom_helper.rb
+++ app/helpers/classroom_helper.rb
@@ -7,5 +7,12 @@ module ClassroomHelper
     end
   end
 
-
+  def student_add_classroom(join_code)
+    if current_user.classrooms.include?(Classroom.find_by_join_code(join_code))
+      redirect_to classroom_index_path
+    else
+      Classroom.add_student_to_class(params[:classroom][:id], params[:classroom][:join_code])
+      redirect_to classroom_index_path
+    end
+  end
 end
