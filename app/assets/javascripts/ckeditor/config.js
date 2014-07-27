d015437e3bcc03a282f9c21f958672fbfbd2eed1
brennontwilliams <brennonw@gmail.com>
2014-07-27 10:37:27 -0700

    Setup for ckeditor
    Added ckeditor to Gemfile.
    Created ckeditor folder in app/assets/javascripts to house config.js. Can also add config.css (to same folder) if more aesthetic customization of the text editor is needed.
    Added requirements in application.js and congfiguration settings in application.rb.

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,9 @@
+// Dimensions
+CKEDITOR.config.height = 600
+CKEDITOR.config.width = 700
+
+// Functionality
+CKEDITOR.config.toolbar = [[ 'Bold', 'Italic', 'Underline'],];
+
+// Aesthetics
+CKEDITOR.config.uiColor = "#AADC6E";
