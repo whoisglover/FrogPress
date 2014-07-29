902edf5eca990904c29e0f561f7f61276a0b13b9
Amelia Downs & Matthew Kuzio <pair+adowns01.mkuzio@devbootcamp.com>
2014-07-29 16:39:54 -0700

    starting to work on xes

--- app/assets/javascripts/edit_buttons.js
+++ app/assets/javascripts/edit_buttons.js
@@ -6,15 +6,24 @@ $(document).ready(function(){
 
 var showXs = function(e){
   var table = $(e.target).siblings('table')
-  var xElem = $(table).find('i').parent()
+  var elem = $('<td class = "pad-top-15 txt-rt pad-r-5"><i class="fa fa-times ft-sz-1 pad-l-20"></i></td>')
+  var data = table.children('tbody').children('tr')
 
-  if (xElem.hasClass('disp-no')){
-    xElem.removeClass('disp-no')
-  } else {
-    xElem.addClass('disp-no')
+  if (data.children('td').children('i') != [] ){
+    alert("already x's!")
   }
+  else{
+    data.append(elem)
+  }
+
+
+
+  // if (xElem.hasClass('disp-no')){
+  //   xElem.removeClass('disp-no')
+  // } else {
+  //   xElem.addClass('disp-no')
+  // }
 }
 
 
-// <td class = "pad-top-15 txt-rt disp-no pad-r-5"><i class="fa fa-times ft-sz-1 pad-l-20"></i></a>
-//         </td>
+ // <td class = "pad-top-15 txt-rt disp-no pad-r-5"><i class="fa fa-times ft-sz-1 pad-l-20"></i></a></td>
