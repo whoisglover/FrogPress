ce229186c1b849c7619a6e48422a4896f9a2f798
brennontwilliams <brennonw@gmail.com>
2014-07-29 13:46:22 -0700

    Refactored Assignment model's self.create_submission_data method
    Also made some small changes self.find_submission_and_status for readability and Ruby convention.

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -24,7 +24,7 @@ class AssignmentController < ApplicationController
 
     if current_user.user_type == "student"
       @completed_submission = Assignment.find_submission_and_status(current_user, @assignment)
-      @submission_data = Assignment.create_submission_data(@completed_submission)
+      @submission_data = Assignment.create_submission_data(current_user, @completed_submission, @assignment)
       @submission = @submission_data[:submission]
       @sub_title_placeholder = @submission_data[:sub_title_placeholder]
       @sub_content_placeholder = @submission_data[:sub_content_placeholder]
