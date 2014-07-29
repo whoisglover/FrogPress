ab46f2dc51617cd73780ce8a79845ee8cdb2c6cf
Joshua Rosaaen <joshua.rosaaen@gmail.com>
2014-07-28 18:45:52 -0700

    spaced it out so its easier to read and hopefully easier to understand

--- spec/features/classroom_features_spec.rb
+++ spec/features/classroom_features_spec.rb
@@ -23,37 +23,40 @@
             # I should go to that past assignment
 
   # As a teacher logged into my classroom (these tests are within the confines of the classroom resource)
+
     # I should see
       # My classes
       # My pending assignments
         # sorted by closest due date
-      # I should be able to
-        # Click on a class and go to that classroom
-          # I should see
+      # The classroom invitation code at the bottom of the page
 
-            # A button to create a new assignment
-              # I can click on create new assignment
-                # It goes to a page (outside classroom resource(/assignment/new/?classroom_id=:id)) to allow teacher to create a new assignment
+    # I should be able to
+      # Click on a class and go to that classroom
+        # I should see
 
-            # A class roster for that classroom
-              # I can click on a student
-                # It goes to a page (outside classroom resource (/users/:id)) to see user data and submissions for a classroom
+          # A button to create a new assignment
+            # I can click on create new assignment
+              # It goes to a page (outside classroom resource(/assignment/new/?classroom_id=:id)) to allow teacher to create a new assignment
 
-            # Pending assignments for that class
-              # sorted by closest due date to today
-                # I can click on an assignment and it goes to a page (outside classroom resource(assignment/:id))
-                  # On this page I can see the classroom(back to classroom resource)
-                  # the assignment title and prompt
-                  # the students, level, and assignment status as a link that currently goes nowhere
-                  # an edit assignment button that goes to another page (outside classroom resource(assignment/:id/edit?classroom_id=:id)
+          # A class roster for that classroom
+            # I can click on a student
+              # It goes to a page (outside classroom resource (/users/:id)) to see user data and submissions for a classroom
 
-            # Past assignments for that class
-              # sorted by closest to date today
-                # I can click on a past assignment and it goes to a page (outside classroom resource(assignment/:id))
-                  # On this page I can see the classroom(back to classroom resource)
-                  # the assignment title and prompt
-                  # the students, level, and assignment status as a link that currently goes nowhere
-                  # an edit assignment button that goes to another page (outside classroom resource(assignment/:id/edit?classroom_id=:id)
+          # Pending assignments for that class
+            # sorted by closest due date to today
+              # I can click on an assignment and it goes to a page (outside classroom resource(assignment/:id))
+                # On this page I can see the classroom(back to classroom resource)
+                # the assignment title and prompt
+                # the students, level, and assignment status as a link that currently goes nowhere
+                # an edit assignment button that goes to another page (outside classroom resource(assignment/:id/edit?classroom_id=:id)
+
+          # Past assignments for that class
+            # sorted by closest to date today
+              # I can click on a past assignment and it goes to a page (outside classroom resource(assignment/:id))
+                # On this page I can see the classroom(back to classroom resource)
+                # the assignment title and prompt
+                # the students, level, and assignment status as a link that currently goes nowhere
+                # an edit assignment button that goes to another page (outside classroom resource(assignment/:id/edit?classroom_id=:id)
 
         # Click on a pending assignment
           # It should go to a page showing that assignment with the students it was assigned to (redirect outside of classroom resource)
