d31b77b3f10fdf9a39986866f3400a3922c0f0ec
Joshua Rosaaen <joshua.rosaaen@gmail.com>
2014-07-28 18:18:45 -0700

    creating the basic feature spec files and creating click through user interactions for review by the team to determine what to test and what not to test

---
+++ spec/features/assignment_features_spec.rb
@@ -0,0 +1,8 @@
+# I should see
+          # A title
+          # A prompt
+          # A submission form
+        # I should be able to
+          # Write in the text box
+          # Save a draft
+          # Submit the assignment
