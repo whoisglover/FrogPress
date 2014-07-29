3a4141f3b7537bc7021b6fa79f3cc53d29b718e8
brennontwilliams <brennonw@gmail.com>
2014-07-29 11:18:51 -0700

    Created self.find_submission_and_status assignment model method
    Moved logic relating to finding a student user's submission and that submission's status. Returns the completed submission. If none exists then @completed_submission is nil.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,4 +2,19 @@ class Assignment < ActiveRecord::Base
   belongs_to :classroom
   has_many :submissions
   validates :classroom_id, presence: true
+
+  def self.find_submission_and_status(current_user, assignment)
+    user_submissions = Submission.where(user_id: current_user.id)
+      if user_submissions.length != 0 # User has submissions
+        user_submissions.each do |submission|
+          if submission.assignment_id == assignment.id && submission.status == "incomplete" # Not complete
+            return completed_submission = nil
+          elsif submission.assignment_id == assignment.id  && submission.status == "complete" # Assignment Complete
+            return completed_submission = submission
+          end
+        end
+      else
+        return completed_submission = nil # User has no submissions so this is nil, Not complete
+      end
+  end
 end
