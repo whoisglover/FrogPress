643972d9548923937fe2b7f252a253a7f65df32c
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-29 01:04:17 -0700

    looping and video functionality broken :/

--- app/assets/javascripts/frontpage.js
+++ app/assets/javascripts/frontpage.js
@@ -1,11 +1,76 @@
 $(document).ready(function(){
-var toggle_login = function(){
-$('[data-ui="signin_button"]').click(function(){
+  var frontpageView = new View();
+  frontpageView.init();
 
-$('[data-ui="preclick"]').addClass('disp-none');
-$('[data-ui="postclick"]').fadeIn();
 })
+
+
+
+
+var View = function(){
+  this.video_count = 1
+  this.videoPlayer = document.getElementById('videoPlayer')
+  // this.videoControls = $('[data-ui="videoControls"]')[0]
+  this.playButton = $('[data-ui="playButton"]')[0]
+  this.pauseButton = $('[data-ui="pauseButton"]')[0]
 }
 
-toggle_login();
-})
+
+View.prototype = {
+  init: function(){
+    this.bindEventListeners();
+    $(this.playButton).hide();
+  },
+
+  bindEventListeners: function(){
+    this.bindLoginButton();
+    console.log('binding..');
+    // debugger
+
+
+
+    // this.pauseButton.addEventListener('click', this.pauseVideo.bind(this));
+    // this.playButton.addEventListener('click', this.playVideo.bind(this));
+  },
+
+  bindLoginButton: function(){
+    $('[data-ui="signin_button"]').click(function(){
+      $('[data-ui="preclick"]').addClass('disp-none');
+      $('[data-ui="postclick"]').fadeIn();
+    })
+  },
+  playNextVideo: function(){
+    // $(this.videoControls).hide()
+    debugger
+    this.video_count++;
+    if(this.video_count==5){this.video_count =1};
+    var nextVideo = "/assets/video"+this.video_count+".mp4";
+    this.videoPlayer.src = nextVideo;
+    this.videoPlayer.play();
+    // $(this.videoControls).show()
+  },
+  pauseVideo: function(e){
+    // debugger
+    this.videoPlayer.pause();
+    $(this.pauseButton).hide()
+    $(this.playButton).show()
+    //background dim
+  },
+  playVideo: function(e){
+    this.videoPlayer.play();
+    $(this.playButton).hide()
+    $(this.pauseButton).show()
+  }
+
+}
+
+function alert() {
+  alert('wtf')
+}
+
+
+
+
+
+
+
