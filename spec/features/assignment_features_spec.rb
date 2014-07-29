2e752d48c57c21172a7b666295256393f7cdeedb
Joshua Rosaaen <joshua.rosaaen@gmail.com>
2014-07-28 18:46:53 -0700

    I think that the teacher part of assignments interaction is complete...for now

--- spec/features/assignment_features_spec.rb
+++ spec/features/assignment_features_spec.rb
@@ -1,3 +1,5 @@
+# As a student interacting with an assignment
+
 # I should see
           # A title
           # A prompt
@@ -6,3 +8,53 @@
           # Write in the text box
           # Save a draft
           # Submit the assignment
+
+
+# As a teacher interacting with an assignment for a classroom (assume teacher is logged in, is in a classroom, and is creating a new assignment)
+  # I can create a new assignment
+    # I can create a title
+    # I can change the due date
+      # I cannot set the due date to a date older than today (Can currently do this!!)
+    # I can create a prompt for the students as content
+    # I can submit the new assignment to the correct classroom
+      # the assignment is not available to other classrooms
+      # the assignment is not available to students outside the classroom
+
+  # I can click on a pending assignment
+    # It will take me to the assignment
+      # I will see
+       # I will see the classroom that this assignment originated from
+       # I will see the title of the assignment
+       # I will see the prompt of the assignment
+       # I will see the students for this class working on this assignment
+       # I will see the level of the assignment for each student
+       # I will see the assignment status for each student
+       # I will see a button that allows me to edit the assignment
+      # I will be able to
+        # Click on the classroom and it will take me back to the classroom that the assignment came from
+        # Click on the student name (currently does nothing)
+        # Click on edit assignment button
+          # This allows teacher to edit the assignment
+            # I can change the title
+            # I can change the due date
+              # I cannot change the due date to a date earlier than today(currently you can)
+            # I can change the content
+            # I can submit the assignment(currently says edit...not submit)
+              # I am then returned to the classroom when assignment is submitted
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
