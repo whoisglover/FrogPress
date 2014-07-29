ac0096b01974d0c4b33fb18bd08dc1e02bbf89c2
brennontwilliams <brennonw@gmail.com>
2014-07-29 13:46:22 -0700

    Refactored Assignment model's self.create_submission_data method
    Also made some small changes self.find_submission_and_status for readability and Ruby convention.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -5,8 +5,8 @@ class Assignment < ActiveRecord::Base
 
   def self.find_submission_and_status(current_user, assignment)
     user_submissions = Submission.where(user_id: current_user.id)
-    if user_submissions.length == 0 # User has no submissions
-      return completed_submission = nil
+    if user_submissions.length.zero?
+      return nil
     end
     user_submissions.each do |submission|
       if submission.assignment_id == assignment.id
@@ -19,28 +19,19 @@ class Assignment < ActiveRecord::Base
     end
   end
 
-  def self.create_submission_data(completed_submission)
-    if completed_submission == nil
-      user_submissions = User.find(current_user.id).submissions
-      if user_submissions.length == 0
-        submission = Submission.new
-        sub_title_placeholder = "Your Essay Title"
-        sub_content_placeholder = "Write your essay here!"
-      else
-        user_submissions.each do |sub|
-          if sub.assignment_id == assignment.id
-            submission = sub
-            sub_title_placeholder = submission.sub_title
-            sub_content_placeholder = submission.sub_content
-            break
-          else
-            submission = Submission.new
-            sub_title_placeholder = "Your Essay Title"
-            sub_content_placeholder = "Write your essay here!"
-          end
-        end
-      end
-    end
-    return {submission: submission, sub_title_placeholder: sub_title_placeholder, sub_content_placeholder: sub_content_placeholder}
+  def self.create_submission_data(current_user, completed_submission, assignment)
+    submission_data = {
+      submission: Submission.new,
+      sub_title_placeholder: "Your Essay Title",
+      sub_content_placeholder: "Write your essay here!"
+    }
+    existing_submission = current_user.submissions.detect {|sub| sub.assignment_id == assignment.id} if completed_submission.present?
+    return submission_data if existing_submission.blank?
+
+    {
+      submission: existing_submission,
+      sub_title_placeholder: existing_submission.sub_title,
+      sub_content_placeholder: existing_submission.sub_content
+    }
   end
 end
