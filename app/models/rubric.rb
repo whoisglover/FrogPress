96a2346681af0afe6b744e5fd327ff13de66b6ab
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-29 23:37:29 -0700

    nested working with hardcoded number of categories

--- app/models/rubric.rb
+++ app/models/rubric.rb
@@ -2,5 +2,5 @@ class Rubric < ActiveRecord::Base
   belongs_to :classroom
   has_many :rubric_categories, dependent: :destroy
   accepts_nested_attributes_for :rubric_categories
-  validates :classroom_id, presence: true
+  # validates :classroom_id, presence: true
 end
