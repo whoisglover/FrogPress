abd92cd3dea55f3b51c206d0ef7b6fa2bd032cab
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-29 23:23:36 -0700

    created rubric, rubric_categories objects and controllers and created nested new rubric form
    This commit includes rubrics/new working with nested form for to create a new rubric and categories

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ FrogPress::Application.routes.draw do
   resources :submission, except: :index
   resources :feedback, only: [:index, :create]
   resources :users, only: [:show]
-  resources :rubric, only: [:create, :show, :destroy]
+
+  resources :rubrics do
+    get 'rubric_categories', on: :member
+  end
+
   delete '/classroom/:classid/users/:userid' => 'classroom#remove_student', as: 'remove_student'
 
   post 'submission/:assignment_id' => 'submission#create', as: 'create_submission'
--- db/migrate/20140730045459_create_rubrics.rb
+++ db/migrate/20140730045459_create_rubrics.rb
@@ -1,6 +1,7 @@
 class CreateRubrics < ActiveRecord::Migration
   def change
     create_table :rubrics do |t|
+      t.string :title
       t.integer :classroom_id
       t.timestamps
     end
