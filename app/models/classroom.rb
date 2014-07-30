0b81c3b1bafd7f858f9e38f47906e8d191754ce4
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-30 13:57:30 -0700

    rubric new, show, edit pages are complete

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -2,6 +2,7 @@ class Classroom < ActiveRecord::Base
   has_many :users_classroom
   has_many :users, through: :users_classroom
   has_many :assignments
+  has_many :rubrics
   validates :name, presence: true
   validates :grade_level, presence: true
   validates :join_code, presence: true
