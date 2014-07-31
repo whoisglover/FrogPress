f6c800b383525a1ec987070fc0317a5890820d37
brennontwilliams <brennonw@gmail.com>
2014-07-31 11:52:59 -0700

    updated pages.js precompile

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module FrogPress
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_assets = true
-    config.assets.precompile += ['pages.js']
+    config.assets.precompile += ['javascripts/pages.js']
 
   end
 end
