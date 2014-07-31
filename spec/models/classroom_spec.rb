de1dca726425ff0b20e5ceab2167df703fa80f28
Morgan O'Leary <rmhwow@gmail.com>
2014-07-30 17:56:59 -0700

    first working test for classroom_spec with outline of upcoming tests

--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Classroom do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "assignments" do
+    let(:classroom) {FactoryGirl.create(:classroom)}
+    it "displays all assignments for a given classroom" do
+      5.times {classroom.assignments << FactoryGirl.create(:assignment)}
+      classroom.assignments.first(5).each do |assignment|
+      expect(classroom.all_assignments).to include(assignment)
+      end
+    end
+  end
+    it "returns assignments whose due date is today or in the future" do
+
+    end
+    it "returns the pending assignments in order" do
+    end
+
+  context "student" do
+  end
+  context "teacher" do
+  end
 end
