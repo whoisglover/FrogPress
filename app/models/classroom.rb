2793c5dc087e838b52d675a577754b823e3f5abc
Morgan O'Leary <rmhwow@gmail.com>
2014-07-31 00:11:28 -0700

    changed association from users_classroom to users_classrooms

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,6 +1,6 @@
 class Classroom < ActiveRecord::Base
-  has_many :users_classroom
-  has_many :users, through: :users_classroom
+  has_many :users_classrooms
+  has_many :users, through: :users_classrooms
   has_many :assignments
   validates :name, presence: true
   validates :grade_level, presence: true
