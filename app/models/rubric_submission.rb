4400c82fcea10e296f1cc644700b3716bfc66f2a
Matthew Kuzio & Danny Glover <pair+mkuzio.whoisglover@devbootcamp.com>
2014-07-31 13:22:26 -0700

    rubric scoring and feedback in working state, some edit routes need more logic.

--- app/models/rubric_submission.rb
+++ app/models/rubric_submission.rb
@@ -1,5 +1,6 @@
 class RubricSubmission < ActiveRecord::Base
   belongs_to :feedback
+  validates :rubric_category_id, uniqueness: {scope: :submission_id}
   # validates :submission_id, presence: true
   # validates :rubric_category_id, presence: true
   # validates :score, presence: true
