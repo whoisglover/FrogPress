93447255ab2cfda17a45ebd624e2af9bf0641109
brennontwilliams <brennonw@gmail.com>
2014-07-31 11:10:44 -0700

    Added production configuration to fix asset compilation issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ FrogPress::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.assets.compile = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
